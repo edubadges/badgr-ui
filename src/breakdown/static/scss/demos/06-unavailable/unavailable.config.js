af7240976b5d636dbf79e08ebccb82ccef26adf9
Nathan Nichols <nnichols@concentricsky.com>
2018-01-12 15:39:05 -0800

    new layout module, demo

---
+++ src/breakdown/static/scss/demos/06-unavailable/unavailable.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    context: {
+        titleContext: {
+            text: "Whoops! The server has failed to respond.",
+            modifier: "title-bold title-margin-bottom-2x title-margin-top-4x title-center"
+        },
+        titleContext2: {
+            text: "Please refresh and try again.",
+            modifier: "title-bold title-center"
+        },
+        marginTopContext: {
+            classList:'l-marginTop'
+        }
+    }
+};
