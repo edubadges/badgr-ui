5524ac0c2a15bb1ca5fa7144a464c4d59107b4e6
Nathan Nichols <nnichols@concentricsky.com>
2018-01-15 09:11:45 -0800

    Use emptyillustration pattern.

--- src/breakdown/static/scss/demos/06-unavailable/unavailable.config.js
+++ src/breakdown/static/scss/demos/06-unavailable/unavailable.config.js
@@ -2,13 +2,13 @@ module.exports = {
     context: {
         titleContext: {
             text: "Whoops! <span class='title title-x-linebreak'>The server has failed to respond.</span>",
-            modifier: "title-bold title-center title-is-smallmobile title-line-height-large",
-            element:'h2'
+            modifier: "title-bold title-center title-is-smallmobile title-line-height-large emptyillustration-x-no-margin-bottom",
+            element:'h1'
         },
         titleContext2: {
             text: "Please refresh and try again.",
             modifier: "title-bold title-center title-is-smallmobile title-line-height-large",
-            element:'h2'
+            element:'h1'
         },
         marginTopContext: {
             classList:'l-marginTop'
