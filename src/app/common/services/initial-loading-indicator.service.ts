1affb255492fb8034720cf6e3b202d340b58bd35
Wiggins <wiggins@concentricsky.com>
2018-01-16 08:44:11 -0800

    tweak setTimeout hack for initial loading indicator in production mode
    fixes BGR-1081

--- src/app/common/services/initial-loading-indicator.service.ts
+++ src/app/common/services/initial-loading-indicator.service.ts
@@ -26,7 +26,8 @@ export class InitialLoadingIndicatorService {
 		// The default behavior is to hide the loading message as soon as angular is ready. Here, by using setTimeout, we're
 		// scheduling that for the next angular cycle. If something sets a new promise before the next cycle, however, it
 		// will preempt this default.
-		this.initialLoadedPromise = new Promise(resolve => setTimeout(resolve));
+		// UPDATE: Jan 2018 -- this trick didnt work in production mode, setting timeout to 100ms for smoothing
+		this.initialLoadedPromise = new Promise(resolve => setTimeout(resolve, 100));
 	}
 
 	private hideIndicator() {
