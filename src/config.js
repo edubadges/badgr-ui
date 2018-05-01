5f9ba3bc49499f9a534714e9f8bec9341a4b55b4
Boris Schellekens <b.schellekens@gmail.com>
2018-05-01 09:16:22 +0200

    Added surfconext to social Account profiders in config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,20 @@
-window.config = {
+const queryParams = location.search.substr(1).split("&")
+    .filter(function(p) { return p.length > 0 })
+    .map(function(p) { return p.split('=').map(decodeURIComponent) })
+    .reduce(function(o, p) { o[p[0]]=p[1]; return o; }, {});
 
+window.config = {
+    api: {
+        baseUrl: "http://localhost:9000",
+        integrationEndpoints: ['/v1/badgebook/integrations']
+    },
+    help: {
+        email: "support@badgr.io"
+    },
+    features: {
+        pathwayGraph: true,
+        alternateLandingRedirect: false,
+        socialAccountProviders: ["surf_conext"]
+    },
 };
+
