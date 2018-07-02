983ff8843139fcb35d5bbe70f8df39ad67e41608
Nathan Nichols <nnichols@concentricsky.com>
2018-07-02 13:31:15 -0700

    initial pagination scss

---
+++ src/breakdown/static/scss/modules/pagination/pagination.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preview: '@preview--none',
+  status: 'wip',
+  variants: [
+    {
+      label: 'Default',
+      preview: '@preview--none',
+    },
+    {
+      label: 'Default (Example)',
+      preview: '@preview',
+      view: 'pagination--example.hbs',
+    },
+  ].map(obj => {
+    obj['name'] = obj.label.replace(/[\s+()]/g, '').toLowerCase();
+    return obj;
+  })
+};
