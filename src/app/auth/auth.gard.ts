589398837d4b8f14d48a7fc8d1b10baec88b1cd9
smoquet <you@example.com>
2018-12-18 15:43:42 +0100

    subclasses authguard for modularity

--- src/app/auth/auth.gard.ts
+++ src/app/auth/auth.gard.ts
@@ -4,26 +4,27 @@ import { Observable } from 'rxjs/Observable';
 // import { AuthService } from '../core/auth.service';
 import { UserProfileManager } from "../common/services/user-profile-manager.service";
 
-@Injectable()
-export class IssuerAuthGuard implements CanActivate {
-
+class AuthGuard implements CanActivate {
+	
 	has_permsission : boolean;
+	permission: string;
   // add the service we need
   constructor(
-    // private auth: AuthService,
 		private router: Router,
 		private profileManager: UserProfileManager,
-  ) {}
+		) {}
 
 	hasPermission(profile){
 		var current_user_permissions = JSON.parse(profile.apiModel['user_permissions'])
-		return current_user_permissions.includes('add_issuer');
+		// return current_user_permissions.includes('add_issuer');
+		console.log(this.permission)
+		return current_user_permissions.includes(this.permission);
 	}
 
   canActivate(
-    next: ActivatedRouteSnapshot,
+		next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-		this.profileManager.userProfilePromise
+			this.profileManager.userProfilePromise
 			.then(profile => {
 				this.has_permsission = this.hasPermission(profile)
 				if (!this.has_permsission){
@@ -31,9 +32,18 @@ export class IssuerAuthGuard implements CanActivate {
 					return false
 				}
 			})
-			
 			return true;
+		}
+	}
 
-  }
 
+@Injectable()
+export class IssuerAuthGuard extends AuthGuard{
+	constructor(
+		router: Router,
+		profileManager: UserProfileManager,
+		) {
+		super(router, profileManager)
+		this.permission = 'add_issuer'
+	}
 }
