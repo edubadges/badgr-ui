86e13126af244c6c5f9dbe39043d61aa3b374c8f
smoquet <you@example.com>
2019-07-07 15:02:14 +0200

    removes obsolete password components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,14 +3,11 @@ import { RouterModule } from "@angular/router";
 
 import { UnauthorizedComponent } from "./unauthorized.component";
 import { LogoutComponent } from "./logout.component";
-import { ResetPasswordSent } from "./reset-password-sent.component";
-import { RequestPasswordResetComponent } from "./request-password-reset.component";
 import { BaseLoginComponent } from "./base-login.component";
 import { LoginComponent } from "./login.component";
 import { LoginStaffComponent } from "./login-teacher.component";
 import { BadgrCommonModule, COMMON_IMPORTS } from "../common/badgr-common.module";
 import { CommonEntityManagerModule } from "../entity-manager/entity-manager.module";
-import { ResetPasswordComponent } from "./reset-password.component";
 import { OAuth2AuthorizeComponent } from "./oauth2-authorize.component";
 
 const routes = [
@@ -48,28 +45,6 @@ const routes = [
 		path: "oauth2/authorize",
 		component: OAuth2AuthorizeComponent
 	},
-
-	/* Reset Password */
-	{
-		path: "request-password-reset",
-		component: RequestPasswordResetComponent
-	},
-	{
-		path: "request-password-reset/:email",
-		component: RequestPasswordResetComponent
-	},
-	{
-		path: "reset-password-sent",
-		component: ResetPasswordSent
-	},
-	{
-		path: "change-password",
-		component: ResetPasswordComponent
-	},
-	{
-		path: "change-password/:token",
-		component: ResetPasswordComponent
-	},
 	{
 		path: "**",
 		redirectTo: "login",
@@ -87,9 +62,6 @@ const routes = [
 		BaseLoginComponent,
 		LoginComponent,
 		LoginStaffComponent,
-		RequestPasswordResetComponent,
-		ResetPasswordSent,
-		ResetPasswordComponent,
 		LogoutComponent,
 		OAuth2AuthorizeComponent,
 		UnauthorizedComponent
