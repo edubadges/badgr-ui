4d2e62cc554bf106061c18e37ffb1c13f902c9f1
daan <daniel@bitofpepper.com>
2019-07-08 16:34:21 +0200

    resigning work

--- src/app/public/privacy-policy.component.ts
+++ src/app/public/privacy-policy.component.ts
@@ -4,59 +4,60 @@ import { SystemConfigService } from "../common/services/config.service";
   template: `
   
   <style>
-  h1 {
-    font-size: 29px;
-    margin: 0px 0px 30px 0px;
-  }
-  h2 {
-    font-size: 26px;
-    margin: 20px 0px 20px 0px;
-  }
-  h3 {
-    font-size: 23px;
-    margin: 15px 0px 15px 0px;
-  }
-  h4 {
-    font-size: 20px;
-    margin: 15px 0px 15px 0px;
-  }
-  li {
-    list-style-type: square;
-    list-style-position: inside;
-    padding-left: 30px;
-    margin: 5px 0px 5px 0px;
-  }
-  span, li, table {
-    color: #6c6b80;
-    font-family: "Open Sans", sans-serif;
-    line-height: 130%;
-  }
-  table {
-    width: 100%;
-  	border-width: 1px;
-  	border-color: #a9c6c9;
-  	border-collapse: collapse;
-    margin: 10px 0px 10px 0px;
-  }
-  table th {
-    font-size: 20px;
-  	border-width: 1px;
-  	padding: 8px;
-  	border-style: solid;
-  	border-color: #a9c6c9;
-  }
-  table td {
-  	border-width: 1px;
-  	padding: 8px;
-  	border-style: solid;
-  	border-color: #a9c6c9;
-  }  
+		#privacy-policy  h1 {
+			font-size: 29px;
+			margin: 0px 0px 30px 0px;
+		}
+		#privacy-policy  h2 {
+			font-size: 26px;
+			margin: 20px 0px 20px 0px;
+		}
+		#privacy-policy  h3 {
+			font-size: 23px;
+			margin: 15px 0px 15px 0px;
+		}
+		#privacy-policy  h4 {
+			font-size: 20px;
+			margin: 15px 0px 15px 0px;
+		}
+		#privacy-policy  li {
+			list-style-type: square;
+			list-style-position: inside;
+			padding-left: 30px;
+			margin: 5px 0px 5px 0px;
+		}
+		#privacy-policy  span, li, table {
+			color: #6c6b80;
+			font-family: "Open Sans", sans-serif;
+			line-height: 130%;
+		}
+		#privacy-policy  table {
+			width: 100%;
+			border-width: 1px;
+			border-color: #a9c6c9;
+			border-collapse: collapse;
+			margin: 10px 0px 10px 0px;
+		}
+		#privacy-policy  table th {
+			font-size: 20px;
+			border-width: 1px;
+			padding: 8px;
+			border-style: solid;
+			border-color: #a9c6c9;
+		}
+		#privacy-policy  table td {
+			border-width: 1px;
+			padding: 8px;
+			border-style: solid;
+			border-color: #a9c6c9;
+		}  
   </style>
   
-  <div class="wrap wrap-light l-containerhorizontal">
+  <div class="wrap wrap-light l-containerhorizontal" >
     <br><br><br>
-		
-		<div [innerHTML]="privacyPolicy|MarkdownToHtml"></div>
+		<button class="button" (click)="switchLanguage()">{{ switchLanguageText }}</button>
+		<br><br><br>
+		<div [innerHTML]="getPrivacyPolicy|MarkdownToHtml" id="privacy-policy"></div>
 		<br><br><br>
   </div>
   `,
@@ -64,17 +65,36 @@ import { SystemConfigService } from "../common/services/config.service";
 })
 export class PublicPrivacyPolicyComponent{
 
+	currentLangNl = true;
+	switchLanguageText = 'Bekijk '
+
 	get currentTheme() {
-		return this.configService.currentTheme
+		return this.configService.currentTheme;
 	}
 
-	get privacyPolicy(){
-		return this.configService.currentTheme.privacy_statement
+	get getPrivacyPolicy(){
+		if(this.currentLangNl) {
+			this.switchLanguageText = 'View in English';
+			return this.configService.currentTheme.privacy_statement;
+		}
+		else{
+			this.switchLanguageText = 'Bekijk in het Nederlands';
+			return this.configService.currentTheme.privacy_statement_en;
+		}
 	}
+
+	get switchLanguage(){
+		this.currentLangNl = !this.currentLangNl
+
+
+	}
+
 	constructor(
 		private configService: SystemConfigService,
 
 	) {
 		this.configService = configService;
+		this.currentLangNl = this.currentTheme.dutch_language_codes.includes(this.currentTheme.language_detected);
+
 	}
 }
