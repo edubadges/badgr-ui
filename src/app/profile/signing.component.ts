2429a4cc8bd4b4db58499f1039fc7f5ee469af4d
smoquet <you@example.com>
2019-08-27 22:14:02 +0200

    makes first basic password form

--- src/app/profile/signing.component.ts
+++ src/app/profile/signing.component.ts
@@ -1,21 +1,64 @@
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { ActivatedRoute, Router } from "@angular/router";
 import { SessionService } from "../common/services/session.service";
 import { BaseAuthenticatedRoutableComponent } from "../common/pages/base-authenticated-routable.component";
+import { markControlsDirty } from "../common/util/form-util";
 
 
 @Component({
 	selector: 'signing',
 	template: `
 	<main>
-		Hello
+		<form-message></form-message>
+
+		<div class="l-containerhorizontal l-containervertical l-childrenvertical wrap">
+			
+			<form class="l-form l-form-span"
+						role="form"
+						aria-labelledby="heading-form"
+						[formGroup]="passwordForm"
+						(ngSubmit)="onSubmit(passwordForm.value)"
+						novalidate
+			>
+			</form>
+			<fieldset role="group" aria-labelledby="heading-badgrsignup2">
+				<bg-formfield-text [control]="passwordForm.controls.password"
+																	[label]="'Password (Must be at least 8 characters)'"
+																	fieldType="password"
+																	[errorMessage]="{ required: 'Please enter a password with your ubikey' }"
+				></bg-formfield-text>
+			</fieldset>
+
+
+
+			<div class="l-form-x-offset l-childrenhorizontal l-childrenhorizontal-small l-childrenhorizontal-right">
+				<a [routerLink]="['/']"
+						class="button button-primaryghost"
+						[disabled-when-requesting]="true"
+				>Cancel</a>
+				<button type="submit"
+								class="button"
+								[disabled]="!! addPasswordFinished"
+								(click)="clickSubmit($event)"
+								[loading-promises]="[ addPasswordFinished ]"
+								loading-message="Adding"
+				>Add Yubikey Password</button>
+			</div>
+		</div>
+
 	</main>
 	`
 })
 export class SigningComponent extends BaseAuthenticatedRoutableComponent implements OnInit {
 
+	passwordForm: FormGroup;
+	addPasswordFinished: Promise<any>;
+
+
 	constructor(
+		fb: FormBuilder,
 		router: Router,
 		route: ActivatedRoute,
 		sessionService: SessionService,
@@ -23,5 +66,29 @@ export class SigningComponent extends BaseAuthenticatedRoutableComponent impleme
 	) {
 		super(router, route, sessionService);
 		title.setTitle("Signing - Badgr");
+
+		this.passwordForm = fb.group({
+			'password': [ '', Validators.compose([ Validators.required, passwordValidator ]) ],
+			}, 
+		);
+	}
+
+	onSubmit(formState) {
+		console.log('submit')
+	}
+
+
+	clickSubmit(ev) {
+		if (!this.passwordForm.valid) {
+			ev.preventDefault();
+			markControlsDirty(this.passwordForm);
+		}
+	}
+
+}
+
+function passwordValidator(control: FormControl): { [errorName: string]: any } {
+	if (control.value.length < 8) {
+		return { 'weakPassword': "Password must be at least 8 characters" }
 	}
 }
